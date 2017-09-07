ed0471f786b8b0e143f2033ed62a9de56b9a7c93
saiphanijakuchibholta <31737128+saiphanijakuchibholta@users.noreply.github.com>
2017-09-07 21:08:15 +0530

    Add files via upload

---
+++ src/A.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class A
+{ 
+    void A() /* Line 3 */
+    {
+        System.out.println("Class A"); 
+    } 
+    public static void main(String[] args) 
+    { 
+        new A(); 
+    } 
+}
